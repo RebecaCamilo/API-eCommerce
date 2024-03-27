27d7f8750af6cebdd13136c478db0eee0f44754e
RebecaCamilo <rebecacamilo3@gmail.com>
2024-03-27 00:48:55 -0300

    feat: add service classes

---
+++ src/main/java/com/loja/ecommerce/service/UserDetailServiceImpl.java
@@ -0,0 +1,22 @@
+package com.loja.ecommerce.service;
+
+import com.loja.ecommerce.model.UserAuthenticated;
+import com.loja.ecommerce.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class UserDetailServiceImpl implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByEmail(username)
+                .map(user -> new UserAuthenticated(user))
+                .orElseThrow(() -> new UsernameNotFoundException("User not found: " + username));
+    }
+}
