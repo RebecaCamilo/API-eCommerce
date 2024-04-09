9f859a8301eb937897c281531f6237109d73791e
RebecaCamilo <rebecacamilo3@gmail.com>
2024-04-09 11:05:52 -0300

    build: add oauth2 jose dependency

--- src/main/java/com/loja/ecommerce/controller/AuthenticationController.java
+++ src/main/java/com/loja/ecommerce/controller/AuthenticationController.java
@@ -2,6 +2,7 @@ package com.loja.ecommerce.controller;
 
 import com.loja.ecommerce.service.AuthenticationService;
 import lombok.AllArgsConstructor;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,7 +14,7 @@ public class AuthenticationController {
     private final AuthenticationService authenticationService;
 
     @PostMapping("/authenticate")
-    public String authenticate() {
-        return authenticationService.authenticate();
+    public String authenticate(Authentication authentication) {
+        return authenticationService.authenticate(authentication);
     }
 }
